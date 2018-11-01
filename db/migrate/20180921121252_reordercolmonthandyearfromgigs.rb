0008f99460052466e958e10f5cab64d9586eaf0a
benitoparis <34385338+benitoparis@users.noreply.github.com>
2018-11-01 23:57:18 +0100

    Delete 20180921121252_reordercolmonthandyearfromgigs.rb

--- db/migrate/20180921121252_reordercolmonthandyearfromgigs.rb
+++
@@ -1,7 +0,0 @@
-class Reordercolmonthandyearfromgigs < ActiveRecord::Migration[5.2]
-  def change
-
-    change_column :gigs, :month, :integer, after: :source
-    change_column :gigs, :year, :integer, before: :quantity_interne
-  end
-end
